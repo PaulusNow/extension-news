a4539f21ff154a5a91ec9754a1aa767f39d6807a
Paulus William <98863022+PaulusNow@users.noreply.github.com>
2025-01-28 09:27:22 +0700

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -9,15 +9,42 @@ document.addEventListener("DOMContentLoaded", function () {
       event.preventDefault();
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const currentUrl = tabs[0].url;  // Get the current URL
-        fetch(`http://localhost:5000/get_content?url=${encodeURIComponent(currentUrl)}`)
+        fetch('http://localhost:5000/get_content', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ url: currentUrl })
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          // Handle the response data
+          console.log("Data berhasil diambil");
+          hiddenLink.style.display = "block";
+
+          // Send the body content for hoax detection
+          const bodyContent = data.data.content; // Use the fetched content directly
+          fetch('http://localhost:5000/detect_hoax', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ content: bodyContent })
+          })
           .then(response => response.json())
           .then(data => {
-            // Handle the response data
-            currentUrlElement.textContent = `Teks H1: ${data.data.title}`;
-            bodyContentElement.textContent = `Body Content: ${data.data.content}`;
-            hiddenLink.style.display = "block";
+            // Display the prediction result
+            const predictionResult = data.prediction[0] === 1 ? 'Hoax' : 'Valid';
+            alert(`Prediction: ${predictionResult}`);
           })
           .catch(error => console.error('Error:', error));
+        })
+        .catch(error => console.error('Error fetching content:', error));
       });
     });
   }
